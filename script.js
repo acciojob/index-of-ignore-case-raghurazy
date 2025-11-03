b5aa285f854b8f648809fa583c27fd3daf4bebda
raghurazy <135259028+raghurazy@users.noreply.github.com>
2025-11-03 19:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase and use indexOf
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
 
 // Please do not change the code below
